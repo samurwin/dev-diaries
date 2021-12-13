17132f5521691acc739049d1d5f224e02b800e35
smudesign <urwinsamantha@gmail.com>
2021-12-13 18:57:18 -0500

    login and out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,6 +60,48 @@ router.post('/', (req, res) => {
     });
 });
 
+// login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email!'});
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect Password' });
+            return;
+        }
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in' });
+        })
+    });
+});
+
+// logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
+});
+
 // update user
 router.put('/:id', (req, res) => {
     User.update(req.body, {
