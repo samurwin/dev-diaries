3b4811e8a3c4389e077b41cffdaa707a66431d6e
smudesign <urwinsamantha@gmail.com>
2021-12-16 14:50:03 -0500

    login page styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./views/layouts/main.handlebars", "./views/homepage.handlebars", "./views/partials/post_info.handlebars"],
+  content: ["./views/layouts/main.handlebars", 
+  "./views/homepage.handlebars", 
+  "./views/partials/post_info.handlebars", 
+  "./views/login.handlebars"],
   theme: {
     extend: {},
   },
