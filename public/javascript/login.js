aa92418ac49dc85469f00f45317fe0864c69bd6b
smudesign <urwinsamantha@gmail.com>
2021-12-16 19:13:06 -0500

    checkmark shows up when password is correct length

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -67,22 +67,19 @@ async function loginFormHandler(event) {
 };
 
 function correctLength(event) {
-    event.preventDefault();
+    // event.preventDefault();
 
-    const checkmark = `<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="rgb(110 231 183 / var(--tw-ring-opacity))">
-    <path fillRule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723
-    3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066
-    0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812
-    3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1
-    1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>`
+    const checkmarkEl = document.createElement('img');
     
-    checkmarkSpanEl.innerHTML = checkmark;
+    checkmarkEl.setAttribute("src", "../assets/checkmark.svg");
+
+    checkmarkSpanEl.append(checkmarkEl);
 };
 
 loginFormEl.addEventListener('submit', loginFormHandler);
 signupFormEl.addEventListener('submit', signupFormHandler);
 signupPasswordEl.addEventListener('input', function() {
-    if (signupPasswordEl.length >= 6) {
+    if (signupPasswordEl.value.length >= 6) {
         correctLength();
     }
 });
