89742fdbcf1de95946807c56d1e33c852fd01b51
smudesign <urwinsamantha@gmail.com>
2022-04-27 20:45:43 -0400

    login runs after signup

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -11,6 +11,8 @@ async function signupFormHandler(event) {
     const email = document.querySelector('input[name="email-signup"]').value.trim();
     const password = document.querySelector('input[name="password-signup"]').value.trim();
 
+    console.log(username, email, password);
+
     if(username && email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
@@ -24,6 +26,21 @@ async function signupFormHandler(event) {
 
         if (response.ok) {
             console.log('Account Created');
+
+            const response = await fetch('api/users/login', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email,
+                    password
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+    
+            if (response.ok) {
+                document.location.replace('/');
+            } else {
+                alert(response.statusText);
+            }
         } else {
             alert(response.statusText);
         }
