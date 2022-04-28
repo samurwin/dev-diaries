25ee3a597d1368d745320e3319b4c3438f9982b8
smudesign <urwinsamantha@gmail.com>
2022-04-27 20:30:01 -0400

    comments fixed

--- public/javascript/add-comment.js
+++ public/javascript/add-comment.js
@@ -2,18 +2,21 @@ const commentFormEl = document.getElementById('add-comment');
 
 async function commentFormHandler(event) {
     event.preventDefault();
+    event.stopImmediatePropagation();
 
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
     const comment_body = document.getElementById('comment-body').value.trim();
 
+    console.log(post_id, comment_body);
+
     const response = await fetch('/api/comment', {
         method: 'POST',
-        body: {
+        body: JSON.stringify({
             post_id,
             comment_body
-        },
+        }),
         headers: { 'Content-Type': 'application/json' }
     })
 
